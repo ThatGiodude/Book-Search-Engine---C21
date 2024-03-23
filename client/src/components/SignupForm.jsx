15fac91aa7d6355786986a3b3e02cc01cf35aabd
ThatGiodude <giovannifstrangio@gmail.com>
2024-03-23 17:04:45 -0400

    fixed server.js requirements

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { ADD_USER } from '../utils/mutations';
-import { createUser } from '../utils/API';
+import createUser from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -12,8 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
   
-  const [addUser] = useMutation(ADD_USER);
-
+  const createUser  = useMutation(ADD_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
