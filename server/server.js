1654fcfb11fd7e5645a9233c9ead454ce00f7782
ThatGiodude <giovanniFStrangio@gmail.com>
2024-03-27 13:53:11 -0400

    added npmrc

--- server/server.js
+++ server/server.js
@@ -25,12 +25,10 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-}
-
- app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  app.get( '*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
  });
-
+}
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
